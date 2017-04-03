79407395e83a0b28b81d57a7ecd60a602f234f31
Emma Cameron <emmakc@gmail.com>
2017-04-03 16:35:19 +1000

    Network setup Client functionality

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -30,6 +30,8 @@ bool Client::startup() {
 										  getWindowWidth() / (float)getWindowHeight(),
 										  0.1f, 1000.f);
 
+	// CONNECT TO SERVER NETWORK
+	handleNetworkConnection();
 
 	return true;
 }
@@ -53,6 +55,9 @@ void Client::update(float deltaTime) {
 
 	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
 		quit();
+
+	// NETWORK MESSAGES
+	handleNetworkMessages();
 }
 
 void Client::draw() {
@@ -66,5 +71,71 @@ void Client::draw() {
 										  0.1f, 1000.f);
 
 	Gizmos::draw(m_projectionMatrix * m_viewMatrix);
+}
+
+void Client::handleNetworkConnection()
+{
+	// Initialize RakNet peer interface first
+	m_pPeerInterface = RakNet::RakPeerInterface::GetInstance();
+	initaliseClientConnection();
+}
+
+void Client::initaliseClientConnection()
+{
+	// CREATE SOCKET DESCRIPTOR
+	// No data needed, as we will be connecting to a server
+	RakNet::SocketDescriptor sd;
+
+	// STARTUP: Max 1 connection to server
+	m_pPeerInterface->Startup(1, &sd, 1);
+
+	std::cout << "Connecting to server at: " << IP << std::endl;
+
+	// CONNECT TO GIVEN SERVER
+	RakNet::ConnectionAttemptResult res = m_pPeerInterface->Connect(IP, PORT, nullptr, 0);
+
+	// CHECK CONNECTION || THROW ERROR
+	if (res != RakNet::CONNECTION_ATTEMPT_STARTED)
+	{
+		std::cout << "Unable to start connection, Error number: " << res << std::endl;
+	}
+}
+
+void Client::handleNetworkMessages()
+{
+	RakNet::Packet* packet;
+
+	for (packet = m_pPeerInterface->Receive(); packet;
+		m_pPeerInterface->DeallocatePacket(packet),
+		packet = m_pPeerInterface->Receive())
+	{
+		switch (packet->data[0])
+		{
+		case ID_REMOTE_DISCONNECTION_NOTIFICATION:
+			std::cout << "Another client has disconnected. \n";
+			break;
+		case ID_REMOTE_CONNECTION_LOST:
+			std::cout << "Another client has lost the connection. \n";
+			break;
+		case ID_REMOTE_NEW_INCOMING_CONNECTION:
+			std::cout << "Another client has connected. \n";
+			break;
+		case ID_CONNECTION_REQUEST_ACCEPTED:
+			std::cout << "Our connection request has been accepted. \n";
+			break;
+		case ID_NO_FREE_INCOMING_CONNECTIONS:
+			std::cout << "The server is full. \n";
+			break;
+		case ID_DISCONNECTION_NOTIFICATION:
+			std::cout << "We have been disconnected. \n";
+			break;
+		case ID_CONNECTION_LOST:
+			std::cout << "COnnection lost. \n";
+			break;
+		default:
+			std::cout << "Received a message with an unkown id: " << packet->data[0];
+			break;
+		}
+	}
 }
 
--- Client/Client.h
+++ Client/Client.h
@@ -3,6 +3,10 @@
 #include "Application.h"
 #include <glm/mat4x4.hpp>
 
+#include <RakPeerInterface.h>
+#include <MessageIdentifiers.h>
+#include <BitStream.h>
+
 class Client : public aie::Application {
 public:
 
@@ -15,7 +19,20 @@ public:
 	virtual void update(float deltaTime);
 	virtual void draw();
 
+	// NETWORK
+	// SERVER CONNECTION
+	void handleNetworkConnection();
+	void initaliseClientConnection();
+	// LISTEN FOR MESSAGES
+	void handleNetworkMessages();
+
 protected:
+
+	// NETWORK
+	RakNet::RakPeerInterface* m_pPeerInterface;
+
+	const char* IP = "127.0.0.1";
+	const unsigned short PORT = 5456;
 
 	glm::mat4	m_viewMatrix;
 	glm::mat4	m_projectionMatrix;
